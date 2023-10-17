280a6d49d905e8a384092c31080ffaaead2d772a
dedvds <dedvandersluijs@gmail.com>
2023-10-17 09:24:07 +0200

    New changes

---
+++ lib/constants.dart
@@ -0,0 +1,7 @@
+const appwriteEndpoint = 'https://cloud.appwrite.io/v1';
+const appwriteProjectId = '652249985aa2473cc0b8';
+const appwriteDatabaseId = '652249c97235b3097a4a';
+const appwriteCollectionId = '652249d6dc23cab02515';
+const appwriteSelfSigned = true;
+const appwriteCollectionId_groceries='652a8bbcb849729d0275';
+const appwriteCollectionId_plans='652a924dbe1a2f2647dc';
